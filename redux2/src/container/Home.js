c3c9a92f9df6b275f73d4f822a37b81c3841d954
aakashdeveloper <ahanda205@gmail.com>
2020-08-05 08:39:48 +0530

    a

--- redux2/src/container/Home.js
+++ redux2/src/container/Home.js
@@ -1,18 +1,25 @@
 import React ,{Component} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import {latestNews } from '../actions';
+import {latestNews,articleNews,galleryNews } from '../actions';
+import LatestDisplay from '../component/Home/latestDisplay';
+import ArticleDisplay from '../component/Home/articleDisplay';
+import GalleryDisplay from '../component/Home/galleryDisplay';
 
 class Home extends Component{
 
     componentDidMount(){
         this.props.dispatch(latestNews())
+        this.props.dispatch(articleNews())
+        this.props.dispatch(galleryNews())
     }
 
     render(){
         return(
             <div>
-                Home Redux
+                <LatestDisplay ldata={this.props.datalist.latestdata}/>
+                <ArticleDisplay adata={this.props.datalist.articledata}/>
+                <GalleryDisplay gdata={this.props.gallist.gallerydata}/>
             </div>
         )
     }
@@ -22,7 +29,8 @@ class Home extends Component{
 function mapStateToProps(state){
     console.log(state)
     return{
-        datalist:state.article
+        datalist:state.article,
+        gallist:state.gallery
     }
 }
 
---
+++ redux2/src/container/NewsDetails.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class NewsDetails extends Component{
+    render(){
+        return(
+            <div>
+                Details
+            </div>
+        )
+    }
+}
+
+export default NewsDetails;
