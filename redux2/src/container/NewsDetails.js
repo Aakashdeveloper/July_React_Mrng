1eba471c0c37f46f6fef54174dc525488e07a3c9
aakashdeveloper <ahanda205@gmail.com>
2020-08-06 08:53:38 +0530

    A

--- redux2/src/container/NewsDetails.js
+++ redux2/src/container/NewsDetails.js
@@ -1,13 +1,74 @@
 import React,{Component} from 'react';
+import {selectedNews,clearSelectedNews} from '../actions';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import LikeCounter from './LikeCounter';
+
 
 class NewsDetails extends Component{
+    componentDidMount(){
+        this.props.dispatch(selectedNews(this.props.match.params.id))
+    }
+
+    renderDetails = ({selected}) => {
+        if(selected){
+            return selected.map((data) => {
+                return(
+                    <div key={data.id}>
+                        <div className="tags">
+                            <span>
+                                <i className="fa fa-eye"></i>
+                                {data.views}
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-up"></i>
+                                {data.likes[0]}
+                            </span>
+                            <span>
+                                <i className="fa fa-thumbs-down"></i>
+                                {data.likes[1]}
+                            </span>
+                        </div>
+                        <div className="top">
+                            <h2>{data.title}</h2>
+                            <span>Artcile By:{data.author}</span>
+                            <img src={`/images/articles/${data.img}`}/>
+                            <div className="body_news">
+                                {data.body}
+                            </div>
+                            <div>
+                                <LikeCounter
+                                articleId={data.id}
+                                likes={data.likes[0]}
+                                dislikes={data.likes[1]}/>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    }
     render(){
         return(
-            <div>
-                Details
+            <div className="news_container">
+                {this.renderDetails(this.props.articleDetails)}
             </div>
         )
     }
+
+    componentWillUnmount(){
+        this.props.dispatch(clearSelectedNews())
+    }
 }
 
-export default NewsDetails;
+function mapStateToProps(state){
+    return{
+        articleDetails:state.article
+    }
+}
+
+NewsDetails.protoTypes={
+    dispatch:PropTypes.func
+}
+
+export default connect(mapStateToProps)(NewsDetails);
