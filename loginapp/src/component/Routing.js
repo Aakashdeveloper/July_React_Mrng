112d0046a1fd9e1264ff41bc61ff0e74d216d0a7
aakashdeveloper <ahanda205@gmail.com>
2020-08-10 08:49:19 +0530

    A

--- loginapp/src/component/Routing.js
+++ loginapp/src/component/Routing.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import RegisterComponent from './RegisterComponent';
 import LoginComponent from './LoginComponent';
 import UserList from './UserList';
+import Profile from './Profile';
 
 const Routing = () => {
     return(
@@ -13,6 +14,7 @@ const Routing = () => {
                 <Route exact path="/" component={RegisterComponent}></Route>
                 <Route exact path="/login" component={LoginComponent}></Route>
                 <Route path="/list" component={UserList}></Route>
+                <Route path="/profile" component={Profile}></Route>
             </BrowserRouter>
         </div>
     )
